3c9fe8790e30da06b08a05da62bfb5d9dcfef799
Danish <danishriz100@gmail.comgit commitexit>
2021-01-07 14:18:50 -0700

    added list and change toolbar colors

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>All Memories</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>Item 1</ion-item>
+        <ion-item>Item 2</ion-item>
+        <ion-item>Item 3</ion-item>
+        <ion-item>Item 4</ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+} from "@ionic/vue";
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+  },
+};
+</script>
