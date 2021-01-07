61e743805351255e8fa5f2f21e58c36d8e3d4a47
Danish <danishriz100@gmail.comgit commitexit>
2021-01-07 16:26:37 -0700

    adding functionality in details page

--- src/pages/MemoryDetail.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <base-layout page-title="The Details" page-default-back-link="/memories">
-    <h2>The details page</h2>
-  </base-layout>
-</template>
-
-<script>
-export default {};
-</script>
